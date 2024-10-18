fe0522ea67fdc56af1ebcd498a1f591f0488e2e7
BiancaLZC <160909595+BiancaLZC@users.noreply.github.com>
2024-10-18 22:31:37 +0300

    modificare modal

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -13,6 +13,6 @@ export const Overlay = styled.div`
 `;
 
 export const ModalContainer = styled.div`
-  max-width: calc(100vw - 48px);
-  max-height: calc(100vh - 24px);
+  max-width: calc(50vw - 48px);
+  max-height: calc(50vh - 24px);
 `;
