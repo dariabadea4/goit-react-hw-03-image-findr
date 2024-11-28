810545fb929fecbaee25081adcbf7be5662c255b
dariabadea4 <164878313+dariabadea4@users.noreply.github.com>
2024-11-28 16:34:53 +0200

    CMT-1

--- src/components/api.js
+++ src/components/api.js
@@ -4,7 +4,7 @@ const controller = new AbortController();
 const signal = controller.signal;
 
 export async function imagesApi(search, page) {
-  const apiKey = '45702092-33b8a91f47742b78dee2b8364';
+  const apiKey = '45783196-139a151e53248a74253d58f65';
   const url = `https://pixabay.com/api/?q=${search}&page=${page}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=${perPage}`;
   return await fetch(url, { signal }).then(res => res.json());
 }
